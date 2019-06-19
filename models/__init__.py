22d0b88214bd50d1f75e955049ec11b6efa1eaaf
QL111111 <731787474@qq.com>
2019-06-19 18:09:28 +0800

    add code and video

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+#------------------------------------------------------------------------------
+#   Bag of models
+#------------------------------------------------------------------------------
+from models.UNet import UNet
+from models.DeepLab import DeepLabV3Plus
+from models.BiSeNet import BiSeNet
+from models.PSPNet import PSPNet
+from models.ICNet import ICNet
